d7ebb2a4e0845b41781e31790c927f1781dc3aca
Alysha Buselt <AlyshaB@Pennwell.com>
2019-03-03 02:16:07 -0600

    calculator -- updated most of the placeholder code. still need to add the calculation functions

--- basicCalculator/dist/js/script.js
+++ basicCalculator/dist/js/script.js
@@ -5,40 +5,64 @@
     initialNumber: null,
     operator: null,
     waitingForNumber: false
-  };
+  }; //console.log(calculator);
+
   const calculatorButtons = document.querySelectorAll('.calculator-button');
-  let displayValue = calculator.displayValue;
-  let newResult = ''; // Add an event listener to each calculator button
-  // Return the value or action performed
+  const calculatorDisplay = document.getElementById('calculatorDisplay');
+  let displayValue = calculator.displayValue; // Add an event listener to each calculator button
 
   calculatorButtons.forEach(button => {
     button.addEventListener('click', event => {
       const {
         target
       } = event;
-      newResult = target.value.toString();
 
       if (target !== button) {
         return;
       }
 
       if (target.classList.contains('calculate-function')) {
-        console.log("Target: " + 'calculate-function', "| Function: " + target.value);
+        //console.log("Target: " + 'calculate-function', "| Function: " + target.value);
+        if (displayValue === '0') {
+          alert("Please enter a number before attempting to perform a calculation.");
+        }
+
+        return;
       }
 
       if (target.id === 'decimal') {
         //console.log("Target: " + 'decimal', "| ID: " + target.value);
-        return newResult;
+        if (!displayValue.includes('.') && displayValue !== '0') {
+          displayResult(target.value);
+        } else if (!displayValue.includes('.') && displayValue === '0') {
+          displayResult('0' + target.value);
+        } else {
+          alert('There is already a decimal point in this number. Please choose a different function.');
+        }
+
+        return;
       }
 
       if (target.classList.contains('number')) {
-        console.log("Target: " + 'number', "| Value: " + target.value);
-        return newResult;
+        //console.log("Target: " + 'number', "| Value: " + target.value);
+        displayResult(target.value);
+        return;
       }
 
       if (target.classList.contains('modify-value')) {
         //console.log("Target: " + 'modify-value', "| Modify By: " + target.value);
-        return newResult;
+        //console.log(displayValue);
+        if (target.id === 'percentage') {
+          convertToPercentage();
+          return;
+        } else {
+          if (displayValue === '0') {
+            alert("Please enter a number before requesting its additive inverse.");
+          } else {
+            additiveInverse();
+            return;
+          }
+        }
       }
 
       if (target.id === 'clearAll') {
@@ -49,19 +73,36 @@
   });
 
   function clearDisplay() {
-    calculator.displayValue = '0';
+    displayValue = '0';
+    calculator.initialNumber = null;
+    calculator.operator = null;
+    calculator.waitingForNumber = false;
+    calculatorDisplay.innerHTML = displayValue;
   }
 
   function displayResult(newResult) {
-    const calculatorDisplay = document.getElementById('calculatorDisplay');
-    calculatorDisplay.innerHTML = displayValue;
-
-    if (newResult !== undefined) {
+    if (displayValue === '0') {
+      displayValue = newResult;
+      calculator.initialNumber = newResult;
       calculatorDisplay.innerHTML = newResult;
-    } else if (newResult === '0') {
-      calculatorDisplay.innerHTML = newResult;
+    } else {
+      displayValue += newResult;
+      calculator.initialNumber = displayValue + newResult;
+      calculatorDisplay.innerHTML = displayValue;
     }
   }
 
-  displayResult();
+  function convertToPercentage() {
+    displayValue = displayValue / 100;
+    console.log(displayValue);
+    displayValue = displayValue.toString();
+    calculatorDisplay.innerHTML = displayValue;
+  }
+
+  function additiveInverse() {
+    displayValue = -displayValue;
+    console.log(displayValue);
+    displayValue = displayValue.toString();
+    calculatorDisplay.innerHTML = displayValue;
+  }
 })();
